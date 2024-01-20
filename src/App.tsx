e87a1458c0dfb8ef0457ffbb1d434701e64442ac
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2024-01-20 17:41:10 -0500

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -45,12 +45,12 @@ function App() {
   };
 
   function logsClick() {
-    if (isLogOpen) {
-      setIsLogOpen(false)
-    } else {
-      setIsLogOpen(true);
-    }
+    setIsLogOpen(!isLogOpen);
 
+  }
+
+  function micRecording() {
+    setIsMicRecording(!isMicRecording)
   }
 
   const handleMouseUp = () => {
@@ -117,7 +117,7 @@ function App() {
           </div>
 
           <div className='flex flex-col space-y-4'>
-            <button className={`button-div ${isMicRecording ? ' border-8 border-red-600 text-red-600 animate-blinkingRecording' : '' }`} onClick={() => setIsMicRecording(true)}>
+            <button className={`button-div ${isMicRecording && started ? ' border-8 border-red-600 text-red-600 animate-blinkingRecording' : '' }`} onClick={micRecording}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-full h-auto">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
               </svg>
