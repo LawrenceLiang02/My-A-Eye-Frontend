0281d042dfae3a108801e7e7a54795641b2e7ddc
siwuagain <01simon.wu@gmail.com>
2024-01-20 15:04:37 -0500

    changed property names

--- src/models/conversationbody.tsx
+++ src/models/conversationbody.tsx
@@ -2,6 +2,6 @@ import { Message } from "./message";
 import { Image } from "./image";
 export type ConversationBody = {
     pastMessages: Message[];
-    currentPrompt: Message;
-    promptImages: Image[];
+    currentMessage: Message;
+    conversationImages: Image[];
 }
