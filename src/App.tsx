c556d33dd06e21b8d5e496f9d616c2dfbccfa4d7
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2024-01-20 17:28:29 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import * as Components from './components/index'
 import React, { useRef } from 'react';
 import { ConversationBody } from './models/conversationbody';
 import { Image }      from './models/image'
+
 function App() {
   const [started, setStarted] = React.useState(false);
   const [isHolding, setIsHolding] = React.useState(false);
-  const [logsOpen, setLogsOpen] = React.useState(false);
+  const [isLogOpen, setIsLogOpen] = React.useState(false);
+  const [isMicRecording, setIsMicRecording] = React.useState(false);
   const timeoutRef = useRef<number | null>(null);
   const [conversationImages, setImages] = React.useState<Image[]>([])
   const [conversationMsg, setMsg] = React.useState<string[]>([])
@@ -32,12 +34,11 @@ function App() {
   };
 
   function logsClick() {
-    if (logsOpen) {
-      setLogsOpen(false)
-    } else {
-      setLogsOpen(true);
-    }
-    
+    setIsLogOpen(!isLogOpen);
+  }
+
+  function micRecording() {
+   setIsMicRecording(!isMicRecording)
   }
 
   const handleMouseUp = () => {
@@ -69,7 +70,7 @@ function App() {
   return (
     <>
       <div className='overscroll-none overflow-hidden flex flex-row max-h-screen h-screen max-w-screen w-screen bg-red-500 py-10 px-8 bg-stripes space-x-8 overscroll-none'>
-        <div className={`flex flex-row justify-around w-full h-full bg-black rounded-lg border-8 ${started ? `border-red-600`: `border-white`}`}>
+        <div className={`flex flex-row justify-around w-full h-full bg-black rounded-lg border-8 ${started ? `border-red-600 animate-blinkingRecording `: `border-white`}`}>
           <div className={`${started ? ``: `hidden`} h-full w-auto bg-black rounded-lg`}>
             <Components.Camera isShowVideo={started} performCapture={performCapture} updateImages={updateImages} stopCapture={stopCapture} />
           </div>
@@ -88,14 +89,14 @@ function App() {
 
         <div className='flex flex-col space-y-4 justify-between py-10'>
           <div className='flex flex-col space-y-4'>
-            <button className={`button-div ${started ? ` border-opacity-80 border-4 border-green-700 text-green-700` : ``} hover:border-opacity-80 hover:border-4 border-green-700 hover:text-green-700`} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseLeave={handleMouseLeave}>
+            <button className={`button-div ${started ? `  border-8 border-green-700 text-green-700` : ``} hover:border-opacity-80 hover:border-4 border-green-700 hover:text-green-700`} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseLeave={handleMouseLeave}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="w-full h-auto">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9" />
               </svg>
               <p className='button-text'>Power</p>
             </button>
 
-            <button className='button-div' onClick={logsClick}>
+            <button className={`button-div ${isLogOpen ? `  border-8 border-blue-600 text-blue-600` : ``} hover:border-opacity-80 hover:border-4 border-blue-700 hover:text-blue-700`} onClick={logsClick}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-full h-auto">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
               </svg>
@@ -104,7 +105,7 @@ function App() {
           </div>
 
           <div className='flex flex-col space-y-4'>
-            <button className='button-div'>
+            <button className={`button-div ${isMicRecording ? ' border-8 border-red-600 text-red-600 animate-blinkingRecording' : '' }`} onClick={micRecording}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-full h-auto">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
               </svg>
@@ -112,15 +113,17 @@ function App() {
             </button>
             <button className='button-div' onClick={handleNewConvo}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-full h-auto">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" />
               </svg>
+
               <p className='button-text'>New</p>
             </button>
           </div>
           
         </div>
 
-        <div className = {`z-30 flex flex-col items-start justify-end h-full absolute inset-y-0 w-96 min-h-screen py-16 right-0 transform duration-700 ease-out overscroll-none overflow-hidden ${logsOpen ? '-translate-x-40' : 'translate-x-full'}`}>
+        <div className = {`z-30 flex flex-col items-start justify-end h-full absolute inset-y-0 w-96 min-h-screen py-16 right-0 transform duration-700 ease-out overscroll-none overflow-hidden ${isLogOpen ? '-translate-x-40' : 'translate-x-full'}`}>
           <div className=" flex flex-col justify-start items-start w-full h-full bg-white rounded-lg">
             <div className='flex flex-col w-full justify-center items-center h-4'>
               {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,24 @@ module.exports = {
     extend: {
       animation: {
         gradient: 'gradientAnimation 3s linear infinite',
+        blinkingRecording: 'blinkingRecording 2s linear infinite',
       },
       keyframes: {
         gradientAnimation: {
           '0%': {
-            backgroundPosition: '100% 0',
+            backgroundPosition: '10% 0%',
+          },
+          '50%': {
+            backgroundPosition: '100% 100%',
           },
           '100%': {
-            backgroundPosition: '0 0',
+            backgroundPosition: '100% 0',
           },
+        },
+        blinkingRecording: {
+          '0%': { transform: 'scale(1)'},
+          '50%': { borderSize: '3', borderColor: '#f87171'}, // #fca5a5 #f87171
+          '100%': { transform: 'scale(1)'},
         },
       },
     },
